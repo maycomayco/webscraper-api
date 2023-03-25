82bc316348ba487796adcfd1a7d6084f37711207
Mayco Barale <maycobarale@gmail.com>
2023-03-25 15:02:29 -0300

    Update index file to work with routes and controllers files

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const express = require("express");
+import express from "express";
+import sitesRoutes from "./v1/routes/sitesRoutes.js";
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 const app = express();
 
-const axiosClient = axios.create({
-  timeout: 5000,
-});
-
-app.get("/api/newbalance", (req, res) => {
-  // get html
-  axiosClient(
-    "https://www.newbalance.com.ar/hombre/zapatillas/running/trail-running"
-  )
-    .then((resp) => resp.data)
-    .then((data) => {
-      const $ = cheerio.load(data);
-      const jsonResponse = { sitename: "New Balance", products: [] };
-      $(".prd").each((i, el) => {
-        const product = {
-          name: $(el).find(".item-title > a").text().trim(),
-          url: $(el).find(".item-title > a").attr("href"),
-          price: $(el).find(".item-price .price").text(),
-          variant: $(el)
-            .find(".item-img > a")
-            .attr("title")
-            .toLocaleLowerCase(),
-        };
-        jsonResponse.products.push(product);
-      });
-      res.send(jsonResponse);
-    });
-});
+app.use(express.json());
+app.use("/api/v1", sitesRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
